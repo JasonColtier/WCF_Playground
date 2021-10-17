086c58fd9cd53127d221daba4a42cbe1bc5da645
Jason <jasoncoltier@gmail.com>
2021-10-17 12:07:03 +0200

    project setup

---
+++ WFCPlayground/Source/WFCPlayground/WFCPlaygroundGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WFCPlaygroundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WFCPLAYGROUND_API AWFCPlaygroundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
